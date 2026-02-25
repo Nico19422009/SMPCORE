fc2de01c52cdfa6588dc4d20c76346aadd97f409
Nico J <147213029+Nico19422009@users.noreply.github.com>
2026-02-25 21:52:32 +0100

    Delete src/main directory

--- src/main/java/com/smpcore/model/CurrencyDefinition.java
+++
@@ -1,4 +0,0 @@
-package com.smpcore.model;
-
-public record CurrencyDefinition(String id, String singular, String plural, String symbol) {
-}
--- src/main/kotlin/com/smpcore/util/Text.kt
+++
@@ -1,7 +0,0 @@
-package com.smpcore.util
-
-import org.bukkit.ChatColor
-
-object Text {
-    fun c(text: String): String = ChatColor.translateAlternateColorCodes('&', text)
-}
